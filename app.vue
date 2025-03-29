fd1eba2112970dcc0cd3ba539d73bb23599540a5
Kevin Kenfack <kenfackjoelkevin@yahoo.com>
2025-03-29 19:18:15 +0100

    update backgroun

--- app.vue
+++ app.vue
@@ -1,13 +1,20 @@
 <template>
-  <NuxtLoadingIndicator color="#14b8a6" />
-  <AppNavbar />
-  <div class="h-32"></div>
-  <UContainer>
-    <NuxtPage />
-  </UContainer>
-  <div class="h-32"></div>
-  <AppFooter />
+  <div class="min-h-screen bg-gray-100 dark:bg-black 
+    bg-[radial-gradient(rgba(0,0,0,0.15)_1px,transparent_1px)] 
+    dark:bg-[radial-gradient(rgba(255,255,255,0.10)_1px,transparent_1px)] 
+    bg-[size:22px_22px] bg-repeat bg-fixed">
+    
+    <NuxtLoadingIndicator color="#14b8a6" />
+    <AppNavbar />
+    <div class="h-32"></div>
+    <UContainer>
+      <NuxtPage />
+    </UContainer>
+    <div class="h-32"></div>
+    <AppFooter />
+  </div>
 </template>
+
 
 <style>
 .page-enter-active,
