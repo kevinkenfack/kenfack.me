9b40a9c988f618c2cd9f09a01e7c2222b4dd2dfe
Kevin Kenfack <142149608+kevinkenfack@users.noreply.github.com>
2025-05-11 15:29:34 +0100

    Update app.vue

--- app.vue
+++ app.vue
@@ -4,7 +4,7 @@
   <div class="h-32"></div>
   <UContainer>
     <NuxtLayout>
-      <NuxtPage />
+      <NuxtPage :key="$route.fullPath" />
     </NuxtLayout>
   </UContainer>
   <div class="h-32"></div>
@@ -15,11 +15,12 @@
 /* Page transition */
 .page-enter-active,
 .page-leave-active {
-  transition: all 0.2s;
+  transition: opacity 0.3s ease, filter 0.3s ease;
 }
+
 .page-enter-from,
 .page-leave-to {
-  opacity: 5;
+  opacity: 0;
   filter: blur(1rem);
 }
 </style>
